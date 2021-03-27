64a5a64ce3e9246d6b77fae0901c93aff7f2a704
AK <amitpasi2018@gmail.com>
2021-03-27 16:27:07 +0530

    Websites

---
+++ hexadecimalandoctal.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int x;
+    
+    printf("Give the decimal number\n");
+    scanf("%d",&x);
+    printf("decimal Number=%d\n",x);
+    printf("Hexadecimal Number=%x\n",x);
+    printf("Octal Number=%o\n",x);
+
+}
